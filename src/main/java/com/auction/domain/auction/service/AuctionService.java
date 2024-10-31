d1fc23e7e32eeef15f8e03afbf21dde8f905e187
kim-na-ram <snrneh3@naver.com>
2024-10-31 13:23:21 +0900

    test(auction) : test 이후 코드 원복

--- src/main/java/com/auction/domain/auction/service/AuctionService.java
+++ src/main/java/com/auction/domain/auction/service/AuctionService.java
@@ -302,8 +302,7 @@ public class AuctionService {
         Long size = redisTemplate.opsForZSet().zCard(auctionHistoryKey);
         if (bidPrice < auction.getMaxPrice() // 입찰가가 현재 최대 입찰가보다 낮거나
                 || (bidPrice == auction.getMaxPrice() && size != null && size != 0)) { // 최대 입찰가와 같으면
-            // TODO(auction) : 테스트 후 주석 해제
-//            throw new ApiException(ErrorStatus._INVALID_LESS_THAN_MAX_PRICE);
+            throw new ApiException(ErrorStatus._INVALID_LESS_THAN_MAX_PRICE);
         }
     }
 }
