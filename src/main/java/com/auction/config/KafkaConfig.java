b4513b8287c6f4e08df3517116361330ee4f1006
inseooo0 <calmandpassion@korea.ac.kr>
2024-11-03 16:28:18 +0900

    chore(coupon) : 카프카 ec2 서버 접근을 위한 yml 수정

--- src/main/java/com/auction/config/KafkaConfig.java
+++ src/main/java/com/auction/config/KafkaConfig.java
@@ -5,6 +5,7 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
@@ -20,10 +21,13 @@ import java.util.Map;
 @EnableTransactionManagement
 public class KafkaConfig {
 
+    @Value("${kafka.producer.bootstrap-servers}")
+    private String kafkaServer;
+
     @Bean
     public ProducerFactory<String, CouponClaimMessage> producerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         return new DefaultKafkaProducerFactory<>(configProps);
@@ -39,7 +43,7 @@ public class KafkaConfig {
     @Bean
     public ConsumerFactory<String, CouponClaimMessage> consumerFactory() {
         Map<String, Object> configProps = new HashMap<>();
-        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        configProps.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, kafkaServer);
         configProps.put(ConsumerConfig.GROUP_ID_CONFIG, "couponGroup");
         configProps.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         configProps.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, JsonDeserializer.class);
