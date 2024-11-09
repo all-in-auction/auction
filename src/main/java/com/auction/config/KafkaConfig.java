a266487d5278a3523d2c50d972466d78e16ec20e
MOON <moonjs1018@naver.com>
2024-11-09 20:04:42 +0900

    refactor(auction) : kafka 커밋 시점 변경 및 ErrorHandler 추가

--- src/main/java/com/auction/config/KafkaConfig.java
+++ src/main/java/com/auction/config/KafkaConfig.java
@@ -2,8 +2,11 @@ package com.auction.config;
 
 import com.auction.domain.auction.event.dto.RefundEvent;
 import com.auction.domain.coupon.dto.CouponClaimMessage;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
+import org.apache.kafka.clients.producer.RecordMetadata;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.apache.kafka.common.serialization.StringSerializer;
 import org.springframework.beans.factory.annotation.Value;
@@ -11,14 +14,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.config.ConcurrentKafkaListenerContainerFactory;
 import org.springframework.kafka.core.*;
+import org.springframework.kafka.listener.ContainerProperties;
+import org.springframework.kafka.listener.DefaultErrorHandler;
+import org.springframework.kafka.support.ProducerListener;
 import org.springframework.kafka.support.serializer.ErrorHandlingDeserializer;
 import org.springframework.kafka.support.serializer.JsonDeserializer;
 import org.springframework.kafka.support.serializer.JsonSerializer;
+import org.springframework.retry.backoff.FixedBackOffPolicy;
+import org.springframework.retry.policy.SimpleRetryPolicy;
+import org.springframework.retry.support.RetryTemplate;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.util.backoff.FixedBackOff;
 
 import java.util.HashMap;
 import java.util.Map;
 
+@Slf4j
 @Configuration
 @EnableTransactionManagement
 public class KafkaConfig {
@@ -49,12 +60,26 @@ public class KafkaConfig {
         configProps.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         configProps.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, JsonSerializer.class);
         configProps.put(ProducerConfig.PARTITIONER_CLASS_CONFIG, "org.apache.kafka.clients.producer.RoundRobinPartitioner");
+
+        // 모든 레플리카에서 전송 완료 시 ack
+        configProps.put(ProducerConfig.ACKS_CONFIG, "all");
+
         return new DefaultKafkaProducerFactory<>(configProps);
     }
 
     // refund KafkaTemplate 빈 정의
     @Bean
     public KafkaTemplate<String, RefundEvent> refundKafkaTemplate() {
+        KafkaTemplate<String, RefundEvent> kafkaTemplate = new KafkaTemplate<>(refundProducerFactory());
+
+        kafkaTemplate.setProducerListener(new ProducerListener<>() {
+
+            @Override
+            public void onError(ProducerRecord<String, RefundEvent> producerRecord, RecordMetadata recordMetadata, Exception exception) {
+                System.err.println("Message sending failed: " + producerRecord.value());
+                exception.printStackTrace();
+            }
+        });
         return new KafkaTemplate<>(refundProducerFactory());
     }
 
@@ -95,6 +120,8 @@ public class KafkaConfig {
         configProps.put(ErrorHandlingDeserializer.KEY_DESERIALIZER_CLASS, StringDeserializer.class.getName());
         // 신뢰할 수 있는 패키지 설정
         configProps.put(JsonDeserializer.TRUSTED_PACKAGES, "com.auction.domain.auction.event.dto, com.auction.domain.coupon.dto");
+        // 커밋 수동으로 설정
+        configProps.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, "false");
 
         return new DefaultKafkaConsumerFactory<>(configProps);
     }
@@ -102,9 +129,25 @@ public class KafkaConfig {
     // refund KafkaListenerContainerFactory 빈 정의
     @Bean
     public ConcurrentKafkaListenerContainerFactory<String, RefundEvent> refundKafkaListenerContainerFactory() {
-        ConcurrentKafkaListenerContainerFactory<String, RefundEvent> factory =
-                new ConcurrentKafkaListenerContainerFactory<>();
+        ConcurrentKafkaListenerContainerFactory<String, RefundEvent> factory = new ConcurrentKafkaListenerContainerFactory<>();
         factory.setConsumerFactory(refundConsumerFactory());
+        factory.setCommonErrorHandler(refundErrorHandler());
+        factory.getContainerProperties().setAckMode(ContainerProperties.AckMode.MANUAL);
+
         return factory;
+    }
+
+    @Bean
+    // Consumer 로직에서 예외 발생 시 재시도 로직 ErrorHandler
+    public DefaultErrorHandler refundErrorHandler() {
+        DefaultErrorHandler errorHandler = new DefaultErrorHandler((consumerRecord, exception) -> {
+            log.error("[Error] topic = {}, key = {}, value = {}, error message = {}",
+                    consumerRecord.topic(),
+                    consumerRecord.key(),
+                    consumerRecord.value(),
+                    exception.getMessage());
+        }, new FixedBackOff(1000L, 10)); // 1초 간격으로 최대 10번 재시도
+
+        return errorHandler;
     }
 }
--- src/main/java/com/auction/domain/auction/event/consume/KafkaAuctionConsumer.java
+++ src/main/java/com/auction/domain/auction/event/consume/KafkaAuctionConsumer.java
@@ -1,19 +1,15 @@
 package com.auction.domain.auction.event.consume;
 
-import com.auction.domain.auction.event.dto.AuctionEvent;
 import com.auction.domain.auction.event.dto.RefundEvent;
-import com.auction.domain.auction.service.AuctionService;
 import com.auction.domain.deposit.service.DepositService;
 import com.auction.domain.point.service.PointService;
 import com.auction.domain.pointHistory.enums.PaymentType;
 import com.auction.domain.pointHistory.service.PointHistoryService;
 import com.auction.domain.user.entity.User;
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.support.Acknowledgment;
 import org.springframework.stereotype.Component;
 
 @Slf4j
@@ -21,21 +17,20 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class KafkaAuctionConsumer {
 
-    private final ObjectMapper objectMapper;
     private final PointService pointService;
     private final PointHistoryService pointHistoryService;
     private final DepositService depositService;
 
-    @KafkaListener(topics = "refund-point-topic", groupId = "refund-group")
-    public void refundConsumer(RefundEvent refundEvent) {
-        try {
+    @KafkaListener(topics = "refund-point-topic", groupId = "refund-group",
+            containerFactory = "refundKafkaListenerContainerFactory")
+    public void refundConsumer(RefundEvent refundEvent, Acknowledgment ack) {
             log.info("RefundEvent = {}", refundEvent);
-//            RefundEvent refundEvent = objectMapper.readValue(message, RefundEvent.class);
             depositService.deleteDeposit(refundEvent.getUserId(), refundEvent.getAuctionId());
             pointService.increasePoint(refundEvent.getUserId(), refundEvent.getDeposit());
+            log.info("Success point refund : {}", refundEvent.getUserId());
             pointHistoryService.createPointHistory(User.fromUserId(refundEvent.getUserId()), refundEvent.getDeposit(), PaymentType.REFUND);
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
+
+            // 예외 없을 시 커밋
+            ack.acknowledge();
     }
 }
