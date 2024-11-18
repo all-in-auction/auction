4500137e50c88b9dda439ea3a4970e77b11e3d77
inseooo0 <calmandpassion@korea.ac.kr>
2024-11-18 15:27:58 +0900

    hotfix(config): fix feign config for resolve eureka discovery fail

--- src/main/java/com/auction/config/FeignConfig.java
+++ src/main/java/com/auction/config/FeignConfig.java
@@ -1,12 +1,10 @@
 package com.auction.config;
 
 import com.auction.feign.decoder.ApiErrorDecoder;
-import feign.Client;
 import feign.Logger;
 import feign.Request;
 import feign.Retryer;
 import feign.codec.ErrorDecoder;
-import feign.okhttp.OkHttpClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
