07d5560ce7453a5cefbac6fc73846ff5658dcfa5
areum0116 <areum0116@naver.com>
2024-11-18 21:49:22 +0900

    hotfix(notification) : remove subscirbe in login

--- src/main/java/com/auction/domain/auth/service/AuthService.java
+++ src/main/java/com/auction/domain/auth/service/AuthService.java
@@ -52,8 +52,6 @@ public class AuthService {
 
         String bearerToken = jwtUtil.createToken(user.getId(), user.getEmail(), user.getAuthority());
 
-        notificationService.subscribe(user.getId().toString());
-
         return LoginResponseDto.of(bearerToken);
     }
 
