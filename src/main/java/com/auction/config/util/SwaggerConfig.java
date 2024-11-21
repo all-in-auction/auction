d9163964060f0deae4b368b069149ce7c4469922
inseooo0 <calmandpassion@korea.ac.kr>
2024-11-21 19:50:56 +0900

    fix(swagger): fix swagger config

--- src/main/java/com/auction/config/util/SwaggerConfig.java
+++ src/main/java/com/auction/config/util/SwaggerConfig.java
@@ -1,6 +1,10 @@
 package com.auction.config.util;
 
+import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
 import io.swagger.v3.oas.models.servers.Server;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -12,11 +16,30 @@ public class SwaggerConfig {
 
     @Bean
     public OpenAPI customOpenAPI() {
+        String jwt = "JWT";
+        SecurityRequirement securityRequirement = new SecurityRequirement().addList(jwt);
+        Components components = new Components().addSecuritySchemes(jwt, new SecurityScheme()
+                .name(jwt)
+                .type(SecurityScheme.Type.HTTP)
+                .scheme("bearer")
+                .bearerFormat("JWT")
+        );
+
         Server gatewayServer = new Server();
         gatewayServer.setUrl("http://www.all-in-auction.site");
         gatewayServer.setDescription("Gateway Server");
 
         return new OpenAPI()
-                .addServersItem(gatewayServer);
+                .addServersItem(gatewayServer)
+                .addSecurityItem(securityRequirement)
+                .components(components)
+                .info(apiInfo());
+    }
+
+    private Info apiInfo() {
+        return new Info()
+                .title("ALL IN AUCTION - POINT API")
+                .description("API documentation for Point Service")
+                .version("v4");
     }
 }
